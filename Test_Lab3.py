39a699ad7684e8fa3c42c96943813780d3b533a3
me <me@mes-MacBook-Pro.local>
2024-05-02 11:33:40 +0800

    a

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -1,5 +1,5 @@
 import Lab3
-
+import random
 print("Test_Lab3")
 
 
@@ -23,8 +23,17 @@ def test_bubble_sort_descending():
 
 def test_bubble_sort_invalid():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = []
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
-    assert (result == [])
+    assert (result == 0)
+    testarr=[random.randint(0,100) for i in range(11)]
+    assert Lab3.bubble_sort(testarr,1)==1
+    testarr=[random.randint(0,100) for i in range(2)]+["pokemon go sucks"]
+    assert Lab3.bubble_sort(testarr,1)==2
+
+    
+    
+
+
